cdb386c109cf6ffca6e4f95c673e62bc9ff1aa39
Brayan Salcedo <brayan_salcedo@epam.com>
2023-10-24 12:10:02 -0600

    Project initialized

--- src/main/java/com/example/concurrency/ConcurrencyApplication.java
+++ src/main/java/com/example/concurrency/ConcurrencyApplication.java
@@ -1,28 +1,13 @@
 package com.example.concurrency;
 
-import com.example.concurrency.services.ConcurrencyService;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@AllArgsConstructor
-@Slf4j
-public class ConcurrencyApplication implements CommandLineRunner {
-
-    private final ConcurrencyService concurrencyService;
+public class ConcurrencyApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(ConcurrencyApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        // concurrencyService.runWithConcurrentHashMap();
-        // concurrencyService.runWithSynchronizedMap();
-        concurrencyService.runConcurrentModification();
     }
 
 }
